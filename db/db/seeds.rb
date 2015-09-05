820576b58117b3216609a1ece850c701b02e41da
Feifan Zhou <feifan@me.com>
2015-09-05 05:24:02 -0400

    Seed some values

--- db/db/seeds.rb
+++ db/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = [
+  User.create(name: 'Ilan Filonenko', profile_pic: 'http://i.imgur.com/8KtR5g4.jpg'),
+  User.create(name: 'Eric Appel', profile_pic: 'http://static.libsyn.com/p/assets/4/0/b/7/40b79e5360461d35/eric-appel.jpg')
+]
+
+posts = [
+  Post.create(winner_id: user.first.id, loser_id: user.last.id, task: 'Hug a platypus', odds: 5, like_count: 0)
+]
+
+comments = [
+  Comment.create(user_id: user.first.id, text: 'Hahaha')
+]
